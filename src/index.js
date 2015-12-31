10f0eb1258c5bb83757143fca80f9c6cf04b461d
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-31 13:02:53 +0900

    create test file

---
+++ src/index.js
@@ -0,0 +1,15 @@
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+        hogehoge
+      </div>
+    );
+  }
+}
+
+React.render(
+  <CommentBox />,
+  document.getElementById('container')
+);
