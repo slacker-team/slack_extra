68250b178086e433b80d1b21940bc8f0ea52e715
山田　潤 <junyamada107@gmail.com>
2015-12-31 15:47:42 +0900

    change activate script

--- gulpfile.js
+++ gulpfile.js
@@ -26,10 +26,6 @@ gulp.task('clean', function () {
   }
 );
 
-gulp.task('watch', function() {
-  gulp.watch(srcDir + '/**/*.{js,jsx}', ['compile']);
-});
-
 gulp.task('sass', function() {
   // ディレクトリ構造を変更次第passは変更する
   gulp.src('sass/**/*scss')
@@ -41,8 +37,11 @@ gulp.task('sass', function() {
 });
 
 
-gulp.task('start', ['compile'], function(){
+gulp.task('electron-start', function(){
   electron.start();
+})
+
+gulp.task('watch', ['compile'], function(){
   gulp.watch(srcDir + '/**/*.{js,jsx}', ['compile']);
   gulp.watch(['main.js'], electron.restart);
   gulp.watch(['index.html', libDir + '/**/*.{html,js,css}'], electron.reload);
