d7c2f35c6c7e7db28f59037b7c13ab6bd85c123b
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-31 15:50:21 +0900

    change description es6

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 
-var React = require('react');
+import React from 'react';
 
 class CommentBox extends React.Component {
   render() {
     return (
       <div className="commentBox">
         Hello, world! I am a CommentBox.
+        hogehoge
       </div>
     );
   }
